7237bc66faaaf1b91d0e5d086b766c1c4e226f9b
George Zhukov <george@rubygarage.org>
2016-02-26 15:47:48 +0200

    test configs

---
+++ config/initializers/devise.rb
@@ -0,0 +1,3 @@
+# Devise.setup do |config|
+#   config.secret_key = 'b038dadf63ae2e0587256510bc12fac713416dc5d90987b8d4c558ab58bca0d87c73481bd763e8db2645a0dd7f6ff6ca70705d7f6fb707c205abc3238e9e4c9a'
+# end
