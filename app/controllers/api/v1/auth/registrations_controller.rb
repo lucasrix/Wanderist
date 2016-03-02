5b11c61a70ccf1981dba58f79d9e2734a453c1ee
George Zhukov <george@rubygarage.org>
2016-03-02 12:59:40 +0200

    added profile creation docs

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -8,12 +8,17 @@ module Api::V1::Auth
 
     api! 'Email registration. A verification email will be sent to the email address provided.'
     param :email, String, required: true, desc: 'Email address'
+    param :first_name, String, required: true, desc: 'First name'
+    param :last_name, String, required: true, desc: 'Last name'
+    param :photo, File, required: false, desc: 'Photo'
     param :password, String, required: true, desc: 'Password'
     param :password_confirmation, String, required: true, desc: 'Password confirmation'
     example <<-EOS
     POST /api/v1/auth
     {
       "email": "username@example.com",
+      "first_name": "John",
+      "last_name": "Smith",
       "password": "password",
       "password_confirmation": "password"
     }
@@ -21,15 +26,14 @@ module Api::V1::Auth
     {
       "status": "error",
       "data": {
-        "id": null,
-        "provider": "email",
-        "uid": "",
-        "name": null,
-        "nickname": null,
-        "image": null,
-        "email": "username@example.com",
-        "created_at": null,
-        "updated_at": null
+        "user": {
+          "id": null,
+          "provider": "email",
+          "uid": "",
+          "email": "username@example.com",
+          "created_at": null,
+          "updated_at": null
+        }
       },
       "error": {
         "error_messages": [
@@ -44,10 +48,43 @@ module Api::V1::Auth
     }
     EOS
 
+    example <<-EOS
+    POST /api/v1/auth
+    {
+      "email": "username2@example.com",
+      "first_name": "John",
+      "last_name": "Smith",
+      "photo": "{BLOB DATA}"
+      "password": "password",
+      "password_confirmation": "password"
+    }
+    201
+    {
+      "status": "success",
+      "data": {
+        "user": {
+          "id": null,
+          "provider": "email",
+          "uid": "username2@example.com",
+          "email": "username2@example.com",
+          "created_at": "2016-02-22T11:57:34.638Z",
+          "updated_at": "2016-02-22T11:57:34.638Z",
+          "profile": {
+            "id": 1,
+            "first_name": "John",
+            "last_name": "Smith",
+            "about": null,
+            "photo": "{URL}",
+            "home_city": null
+            "personal_url": null
+          }
+        }
+      }
+    }
+    EOS
+
     def create
-      super do |user|
-        user.username = user.email
-      end
+      super
     end
 
     protected
