9acb8b64098c1bedfdc8e0d6f29618a5228cfe4b
Artem Zaslavskiy <artem.zaslavskiy@gmail.com>
2016-04-19 18:59:50 +0300

    Add geocode key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'maplify.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  config.geokit.geocoders.google = ENV['GOOGLE_GEOCODE_KEY']
 end
