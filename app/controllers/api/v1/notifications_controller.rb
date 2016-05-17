3668841c44bbb24facef598d10be975522735d9e
Artem Zaslavskiy <artem.zaslavskiy@gmail.com>
2016-05-17 15:06:00 +0300

    Make unread

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -7,15 +7,20 @@ module Api::V1
     end
 
     load_and_authorize_resource through: :current_user
-    after_action :make_read
+    after_action :make_read, if: :make_read?
 
     api! 'List of notifications'
+    param :make_read, [true, false], required: false, desc: 'Set unread to false'
     def index
       notifications = @notifications.ordered
       render json: Response.new(notifications, scope: current_user)
     end
 
     private
+
+    def make_read?
+      params[:make_read] == true
+    end
 
     def make_read
       @notifications.update_all(unread: false)
