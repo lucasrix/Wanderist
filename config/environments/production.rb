6ec481a47c276745bfcdd62d85baa78e97f3e3ff
Artem Zaslavskiy <artem.zaslavskiy@gmail.com>
2016-08-09 22:40:37 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'maplify.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'api.maplifyapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:  ENV['SENDGRID_USERNAME'],
